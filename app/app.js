e0499354adf5dd53ea90cc6bfa355445bbac87d8
Jan Ladleif <jan.ladleif@hpi.uni-potsdam.de>
2018-11-27 18:22:01 +0100

    Squash all commits from chor-js-demo up until this point
    move over files from chor-js repo
    
    update package json and bpmn files
    
    fix models to conform to metamodel
    
    bump chor-js version
    
    bump chor-js version
    
    bump chor-js version
    
    adapt to chor-js 0.2.1
    
    bump chor-js version
    
    add linter (#7)
    
    Upload xml (#3)
    
    * Add a file selection and nicer download buttons.
    
    Add Travis integration (#8)
    
    * add travis config
    
    * some change
    
    Add Docker support (#10)
    
    * add Docker image
    
    * update README
    
    Fix typo in README (#11)
    
    Remove unnecessary Babel dependency (#12)
    
    * Add a file selection and nicer download buttons.
    
    * Remove babel-es2015 dependency
    
    * fix typo in README
    
    * bump chor-js version
    
    Add link to github and button for new diagram (#16)
    
    * Add a link to github and a button to create a new file
    * Improve alignment of buttons
    * Fix an issue where the old diagram would download if no changes had been made.
    
    Add validator component (#2)
    
    * add initial code for the validator
    
    * Add a file selection and nicer download buttons.
    
    * Add general structure
    
    * Add more structure to the reporter. Also add a file with event based gateways for testing purposes.
    
    * Wip Validator work
    
    * Add popup with error description
    
    * Add useful validation button with continuous validation
    
    * Add enum for warning level
    
    * Fix bug, where validation was enabled by accident.
    
    * WIP. Add ability to get neighbor elements of certain type
    
    * Add warning to Timer Events
    
    * Add annotations for SubChoreos.
    
    * Add duplicate Participant name detection warning
    
    * Add waring for unshown participant bands of sub choreos.
    
    * Add documentation and display errors first
    
    * split validator up into multiple source files
    
    * Remove all flatMaps. Replace with self made flat.
    Add shadow to info windows
    
    * Add button for validation
    
    * Add adaptive title for validator button
    
    * some styling, also errors and warnings that are nested are not shown directly anymore for sub-choreos
    
    Fix wording of simple flow constraint (#20)
    
    Change default choreo, clean up XML (#22)
    
    * add more approachable first example
    
    * remove references to Eclipse
    
    Bump chor-js version
    
    Update README.md
    
    Remove the z-index for validation annotation circles to fix issue #18 (#23)
    
    Fix issue #18
    
    Remove shadow for validator button and slightly rephrase the flow constraint warning. (#24)
    
    Added chor-js css (#25)
    
    Move repository to bptlab (#26)
    
    * prepare moving of the repo
    
    * bump chor-js version
    
    * fix chor-js version
    
    Simplify code for buttons
    
    Bump chor-js version
    
    Fix reloading when renaming a choreography
    
    Adapt for fix/125 in chor-js (#28)
    
    Validate call choreographies (#29)
    
    * WIP Add reporting for violation of bijectivity constraint for call choroe participants and add groundwork for cycle detection of call choreos and
    
    * Disable unfinished cyclic dependency check
    
    * Comment out unfinished code
    
    * uncomment no cyclic call choreo constraint
    
    * remove unused constraint
    
    * some minor phrasing
    
    Bump chor-js version
    
    Restructure (#30)
    
    * First working commit for switching to parcel.
    run parcel app/index.html
    
    * Readd eslint
    
    * Refactor directory structure
    
    * adapt to changed paths in chor-js
    
    * Get restructuring running with grunt. Remove unnecessary parts required by parcel
    
    * Bump chor-js version
    
    Add favicon
    
    Validate call choreo cycles (#32)
    
    * Add feature to debugger that checks for cyclic calls and issues a descriptive warning.
    
    properties panel (#33)
    
    Integrates the bpmn-js-properties-panel with chor-js
    
    remove camunda specific properties (#35)
    
    Removes Camunda specific properties for conditional events and allows specifying a formal expression.
    
    Add sequence flow conditions to properties panel (#36)
    
    Add Item definition configuration for messages (#37)
    
    Allow configuring item definitions for messages, including the data structure and a name for the definition

---
+++ app/app.js
@@ -0,0 +1,154 @@
+import xml from './diagrams/pizzaDelivery.bpmn';
+import blankXml from './diagrams/newDiagram.bpmn';
+import $ from 'jquery';
+import ChoreoModeler from 'chor-js/lib/Modeler';
+import Reporter from './lib/validator/Validator.js';
+import propertiesPanelModule from 'bpmn-js-properties-panel';
+import propertiesProviderModule from './lib/properties-provider';
+
+var modeler = new ChoreoModeler({
+  container: '#canvas',
+  propertiesPanel: {
+    parent: '#properties-panel'
+  },
+  additionalModules: [
+    propertiesPanelModule,
+    propertiesProviderModule
+  ],
+
+  keyboard: {
+    bindTo: document
+  }
+});
+
+renderModel(xml);
+
+function renderModel(newXml) {
+  modeler.importXML(newXml, {
+    // choreoID: '_choreo1'
+  }).then(() => {
+    modeler.get('canvas').zoom('fit-viewport');
+  }).catch(error => {
+    console.error('something went wrong: ', error);
+  });
+}
+
+function saveSVG(done) {
+  modeler.saveSVG(done);
+}
+
+function saveDiagram(done) {
+  modeler.saveXML({ format: true }, function(err, xml) {
+    done(err, xml);
+  });
+}
+
+$(function() {
+  const reporter = new Reporter(modeler);
+  var downloadLink = $('#js-download-diagram');
+  var downloadSvgLink = $('#js-download-svg');
+  const validateButton = $('#js-validate');
+  const panelToggle = $('#panel-toggle');
+  const propertiesPanel = $('#properties-panel');
+
+  panelToggle.click(e => {
+    propertiesPanel.toggle();
+    $('#open-toggle').toggle();
+    $('#close-toggle').toggle();
+  });
+  let isValidating = false;
+
+  validateButton.click(e => {
+    if (!isValidating) {
+      isValidating = true;
+      reporter.validateDiagram();
+      $(e.target).addClass('selected');
+      $(e.target).prop('title', 'Disable checking');
+    } else {
+      isValidating = false;
+      reporter.clearAll();
+      $(e.target).removeClass('selected');
+      $(e.target).prop('title', 'Check diagram for problems');
+
+    }
+  });
+
+  $('.buttons a').click(function(e) {
+    if (!$(this).is('.active')) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+
+  function setEncoded(link, name, data) {
+    var encodedData = encodeURIComponent(data);
+    if (data) {
+      link.addClass('active').attr({
+        'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
+        'download': name
+      });
+    } else {
+      link.removeClass('active');
+    }
+  }
+
+  var exportArtifacts = debounce(function() {
+    saveSVG(function(err, svg) {
+      setEncoded(downloadSvgLink, 'diagram.svg', err ? null : svg);
+    });
+    saveDiagram(function(err, xml) {
+      setEncoded(downloadLink, 'diagram.bpmn', err ? null : xml);
+    });
+  }, 500);
+
+  $('#js-new-diagram').click(function(e) {
+    renderModel(blankXml);
+    exportArtifacts();
+  });
+
+  $('input#file-input').change(function(e) {
+    var reader = new FileReader();
+    var file = document.querySelector('input[type=file]').files[0];
+    reader.addEventListener('load', function() {
+      const newXml = reader.result;
+      renderModel(newXml);
+      exportArtifacts();
+    }, false);
+
+    if (file) {
+      reader.readAsText(file);
+    }
+
+  });
+
+  exportArtifacts();
+  modeler.on('commandStack.changed', exportArtifacts);
+  modeler.on('commandStack.changed', function() {
+    if (isValidating) {
+      reporter.validateDiagram();
+    }
+  });
+  modeler.on('import.render.complete', function() {
+    if (isValidating) {
+      reporter.validateDiagram();
+    }
+  });
+});
+
+// expose bpmnjs to window for debugging purposes
+window.bpmnjs = modeler;
+
+// helpers //////////////////////
+
+function debounce(fn, timeout) {
+
+  var timer;
+
+  return function() {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(fn, timeout);
+  };
+}
